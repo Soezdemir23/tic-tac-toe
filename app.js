ef9f2e80b1d9a0d11cf4d53bcd48c27a156f0aed
Soezdemir23 <Soezdemir3423@gmail.com>
2022-06-04 06:25:41 -0600

    Start adding logic

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ maybe also an gameplan
 
 
 
-const displayController = (() =>{
+const displayController = (() => {
     const gameBoard = []
     const gameBoardDraw = document.querySelectorAll('.grid-child')
-    const gameBoardArrayManipulation = (sign, index) => {
+    const gameBoardArrayAndBoardManipulation = (sign, index) => {
         // if the index is -1, turn the whole array to empty strings and the
         // gameboard to "" as well.
         if (index === -1) {
@@ -19,13 +19,13 @@ const displayController = (() =>{
                 element.textContent = "";
                 gameBoard.pop();
             })
-        // if the player is entering an x or o, then simply enter it in the  respective place.
-        } else if (sign === "X" || sign === "O" ){
+            // if the player is entering an x or o, then simply enter it in the  respective place.
+        } else if (sign === "X" || sign === "O") {
             gameBoard[index] = sign;
             gameBoardDraw[index].textContent = sign;
-        } 
+        }
     }
-    return {gameBoard,gameBoardDraw, traverseBoard, gameBoardArrayManipulation}
+    return { gameBoard, gameBoardDraw, traverseBoard, gameBoardArrayAndBoardManipulation }
 })()
 
 /**
@@ -36,17 +36,76 @@ const displayController = (() =>{
  *  Then check if the clicked fields are already given an x or o. 
  */
 const gameRules = (() => {
-    const fullFieldChecker = (gameBoard) {
+    // raise the round number, when each player has done their part
+    let roundCount = 0;
+    const roundIncrementer = () => {
+        console.log(`Round: ${roundCount}`)
+        roundCount++;
+    }
+    // check if the field is full, returns false if not, else true
+    const fullFieldChecker = (gameBoard) => {
         let count = 0
         gameBoard.forEach(element => {
-            if ()
+            if (element.textContent != "") {
+                count++;
+            }
         });
+        if (count == 9) console.log("the field is full");
+        else console.log("The field is not full")
     }
-})
+
+    // rules for winning are, 3 crosses, or else check if the field is full, therefore a draw.
+    const winConditionChecker = (gameBoard) => {
+        // check if three rounds have been already played, else skip
+        if (roundCount > 3) {
+            if (// line
+                gameBoard[0] == "X" && gameBoard[1] == "X" && gameBoard[2] == "X" ||
+                gameBoard[3] == "X" && gameBoard[4] == "X" && gameBoard[5] == "X" ||
+                gameBoard[6] == "X" && gameBoard[7] == "X" && gameBoard[8] == "X" ||
+                // column
+                gameBoard[0] == "X" && gameBoard[3] == "X" && gameBoard[6] == "X" ||
+                gameBoard[1] == "X" && gameBoard[4] == "X" && gameBoard[7] == "X" ||
+                gameBoard[2] == "X" && gameBoard[5] == "X" && gameBoard[8] == "X" ||
+                //diagonal
+                gameBoard[0] == "X" && gameBoard[4] == "X" && gameBoard[8] == "X" ||
+                gameBoard[0] == "X" && gameBoard[1] == "X" && gameBoard[2] == "X"
+            ) {
+                // we can add a modal here instead
+                console.log("player one won")
+            }
+            else if (
+                gameBoard[0] == "O" && gameBoard[1] == "O" && gameBoard[2] == "O" ||
+                gameBoard[3] == "O" && gameBoard[4] == "O" && gameBoard[5] == "O" ||
+                gameBoard[6] == "O" && gameBoard[7] == "O" && gameBoard[8] == "O" ||
+                // column
+                gameBoard[0] == "O" && gameBoard[3] == "O" && gameBoard[6] == "O" ||
+                gameBoard[1] == "O" && gameBoard[4] == "O" && gameBoard[7] == "O" ||
+                gameBoard[2] == "O" && gameBoard[5] == "O" && gameBoard[8] == "O" ||
+                //diagonal
+                gameBoard[0] == "O" && gameBoard[4] == "O" && gameBoard[8] == "o" ||
+                gameBoard[0] == "O" && gameBoard[1] == "O" && gameBoard[2] == "O"
+
+            ) {
+                // sae as above
+                console.log("player two won")
+            } else {
+                console.log("Game is running sstill")
+            }
+        }
+    }
+})()
+/**
+ * Get 
+ */
+const game= (() => {
+    for 
+}
 
 
-const playerFactory = (name, sign) =>{
-    return {name, sign};
+
+
+const playerFactory = (name, sign) => {
+    return { name, sign };
 }
 
 
@@ -55,6 +114,6 @@ const playerFactory = (name, sign) =>{
 
 
 const computer = () => {
-    const {sign, name} = Person(sign, name);
+    const { sign, name } = Person(sign, name);
     const
 }
