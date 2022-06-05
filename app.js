eb0b06e437450769c8a624c6d17b5e532dbc7620
Soezdemir23 <soezdemir3423@gmail.com>
2022-06-05 09:38:44 +0200

    Add more logic to the game

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ ruleChecker object
 maybe also an gameplan
 */
 
+const playerFactory = (name, sign) => {
+    return { name, sign };
+}
 
 
 const displayController = (() => {
@@ -25,7 +28,7 @@ const displayController = (() => {
             gameBoardDraw[index].textContent = sign;
         }
     }
-    return { gameBoard, gameBoardDraw, traverseBoard, gameBoardArrayAndBoardManipulation }
+    return { gameBoard, gameBoardDraw, gameBoardArrayAndBoardManipulation }
 })()
 
 /**
@@ -54,6 +57,11 @@ const gameRules = (() => {
         else console.log("The field is not full")
     }
 
+    const oneFieldChecker = (player, dataSetNr) => {
+        if (displayController.gameBoard[datasetNr] !== "") return;
+        displayController.gameBoardArrayAndBoardManipulation(player.sign, dataSetNr);
+    }
+
     // rules for winning are, 3 crosses, or else check if the field is full, therefore a draw.
     const winConditionChecker = (gameBoard) => {
         // check if three rounds have been already played, else skip
@@ -71,7 +79,7 @@ const gameRules = (() => {
                 gameBoard[0] == "X" && gameBoard[1] == "X" && gameBoard[2] == "X"
             ) {
                 // we can add a modal here instead
-                console.log("player one won")
+                return 1
             }
             else if (
                 gameBoard[0] == "O" && gameBoard[1] == "O" && gameBoard[2] == "O" ||
@@ -82,38 +90,90 @@ const gameRules = (() => {
                 gameBoard[1] == "O" && gameBoard[4] == "O" && gameBoard[7] == "O" ||
                 gameBoard[2] == "O" && gameBoard[5] == "O" && gameBoard[8] == "O" ||
                 //diagonal
-                gameBoard[0] == "O" && gameBoard[4] == "O" && gameBoard[8] == "o" ||
+                gameBoard[0] == "O" && gameBoard[4] == "O" && gameBoard[8] == "O" ||
                 gameBoard[0] == "O" && gameBoard[1] == "O" && gameBoard[2] == "O"
 
             ) {
                 // sae as above
-                console.log("player two won")
+                return 2
             } else {
-                console.log("Game is running sstill")
+                return 0
             }
         }
     }
+    return {winConditionChecker, oneFieldChecker, fullFieldChecker, roundIncrementer}
 })()
 /**
- * Get 
+ * Get all these things together.
+ * 1. Clear the room, for new game
+ * 2. check whose turn it is
+ * 3. start the insertion
  */
 const game= (() => {
-    for 
-}
+    let section = document.querySelector('section')
+    // if first player's turn, then say true. else false
+    let whoseTurn = true;
+
+    // just a simple turnchanger
+    let changeturn = ()  => {
+        if (whoseTurn == false) {
+            whoseTurn = true;
+        }else {
+            whoseTurn = false
+        }
+    }
+
+    let clear = () => {displayController.gameBoardArrayAndBoardManipulation("", -1)}
+    // we can start here, only run this from the comfort of the value
+    let gameStart = () => {
+        // first create two players
+        let playerOne = playerFactory("One", "X");
+        let playerTwo = playerFactory("Two", "O")
+        // we do NOT need a while loop. THIS is our while loop. My god I absolutely forgot that.
+        // I also need to extract some code from here when I am done
+        section.onclick = (event) => { 
+            // get the necessary html event
+            let gridItem = event.target;
+            // get the data-attribute
+            let index = parseInt(gridItem.dataset.nr)
+            console.log(index)
+            // this is actually not necessary, I am just too defensive about this
+            if (index >= 0 && index <= 8 ) {
+                //check if the grid is empty. if it is, it should change it.
+                gameRules.oneFieldChecker(player, index);
+                changeturn();
+
+
+            } else {
+                console.log("field full: ", displayController.gameBoard[index])
+            }
+        }
+    }
+
+    let steps = (player, datasetNr) => {
+        if(whoseTurn == true){
+            if(player.sign !=="X") return
+            displayController.gameBoardArrayAndBoardManipulation(player, datasetNr)
+        }
+    }
+
+     return {clear, gameStart, changeturn
+     }
+})()
+
+game.gameStart()
 
 
 
 
-const playerFactory = (name, sign) => {
-    return { name, sign };
-}
 
 
 
-
-
-
+/**
+ * Needs to be expanded upon when I am done with the basic functionality above.
+ * Looking forward and scare of the minmax implementation.
+ */
 const computer = () => {
     const { sign, name } = Person(sign, name);
-    const
+
 }
